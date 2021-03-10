d94b76d24165a3d719dd57fb0361195342336c9a
almoghr <almoghr36@gmail.com>
2021-03-10 11:46:58 +0200

    added a function to generate number + started to work on gameScreen component

--- guessANumberApp/App.js
+++ guessANumberApp/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import Header from './components/Header'
-import StartGameScreen from './screens/StartGameScreen'
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen />;
   return (
-  <View style={styles.screen}>
-    <Header title="Guess A Number App" />
-    <StartGameScreen />
-  </View>)
+    <View style={styles.screen}>
+      <Header title="Guess A Number App" />
+      {!userNumber ? (
+        <StartGameScreen onStartGame={startGameHandler} />
+      ) : (
+        <GameScreen />
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  }
+  },
 });
