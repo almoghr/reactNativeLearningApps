344aa1cbc855a0e4e52818ddde22d51de78d7a3b
almoghr <almoghr36@gmail.com>
2021-03-11 10:17:05 +0200

    added dimensions API to the gameover screen which gave a reachable screen, also added scroll view in case there are too small phones

--- guessANumberApp/screens/GameOverScreen.js
+++ guessANumberApp/screens/GameOverScreen.js
@@ -1,30 +1,39 @@
 import React from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+} from "react-native";
 import Colors from "../constants/colors";
 import MainButton from "../components/MainButton";
 const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>The Game is Over!</Text>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          // source={require("../assets/success.png")}
-          source={{
-            uri:
-              "https://media.istockphoto.com/photos/businessman-standing-on-mountain-peak-picture-id1082721304?k=6&m=1082721304&s=612x612&w=0&h=Qpir2I76ZejZlu5qMUTZqGRmXjMGfPaB6z4CO-McZNo=",
-          }}
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <Text style={styles.title}>The Game is Over!</Text>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            // source={require("../assets/success.png")}
+            source={{
+              uri:
+                "https://media.istockphoto.com/photos/businessman-standing-on-mountain-peak-picture-id1082721304?k=6&m=1082721304&s=612x612&w=0&h=Qpir2I76ZejZlu5qMUTZqGRmXjMGfPaB6z4CO-McZNo=",
+            }}
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>
+            Your Phone needed{" "}
+            <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess
+            the number <Text style={styles.highlight}>{userNumber}</Text>
+          </Text>
+        </View>
+        <MainButton onPress={onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>
-          Your Phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
-          rounds to guess the number{" "}
-          <Text style={styles.highlight}>{userNumber}</Text>
-        </Text>
-      </View>
-      <MainButton onPress={onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -35,13 +44,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30,
+    marginVertical: Dimensions.get("window").height / 20,
   },
   image: {
     width: "100%",
@@ -52,14 +61,13 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   resultContainer: {
-      marginHorizontal: 30,
-      marginVertical: 15
-
+    marginHorizontal: Dimensions.get("window").height / 20,
+    marginVertical: Dimensions.get("window").height / 40,
   },
   resultText: {
     fontFamily: "open-sans",
     textAlign: "center",
-    fontSize: 20,
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20,
   },
   highlight: {
     color: Colors.primary,
--- guessANumberApp/screens/GameScreen.js
+++ guessANumberApp/screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Button,
   Alert,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -96,7 +97,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 350,
     maxWidth: "90%",
   },
