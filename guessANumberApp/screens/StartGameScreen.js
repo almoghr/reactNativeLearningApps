b5766c241be7c74f45fdb6d156c979b836a9b72d
almoghr <almoghr36@gmail.com>
2021-03-11 10:05:06 +0200

    added Dimensions API to the buttons - to be responsive

--- guessANumberApp/screens/StartGameScreen.js
+++ guessANumberApp/screens/StartGameScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from "react-native";
 import Card from "../components/Card";
 import Input from "../components/Input";
@@ -106,7 +107,7 @@ const styles = StyleSheet.create({
     fontFamily: "open-sans-bold"
   },
   inputContainer: {
-    Width: "80%",
+    width: "80%",
     maxWidth:'95%',
     minWidth:300,
     alignItems: "center",
@@ -118,7 +119,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 100,
+    width: Dimensions.get('window').width / 4,
   },
   input: {
     width: 50,
